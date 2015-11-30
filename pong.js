e0cad3df857aef9029b5328271058ed40e5cddb3
Charlie <charlie.feng@mail.mcgill.ca>
2015-11-30 12:47:14 -0500

    paddle movements

--- pong.js
+++ pong.js
@@ -24,7 +24,36 @@ var step = function() {
 };
 
 var update = function() {
+	player.update();
 	ball.update(player.paddle, computer.paddle);
+
+};
+
+Player.prototype.update = function() {
+	for (var key in keysDown) {
+		var value = Number(key);
+		if(value == 37) {
+			this.paddle.move(-4, 0);
+		} else if (value == 39) {
+			this.paddle.move(4, 0);
+		} else {
+			this.paddle.move(0, 0);
+		}
+	}
+};
+
+Paddle.prototype.move = function(x, y) {
+	this.x += x;
+	this.y += y;
+	this.x_speed = x;
+	this.y_speed = y;
+	if (this.x < 0) { // all the way to the left
+		this.x = 0;
+		this.x_speed = 0;
+	} else if (this.x + this.width > 400) { // all the way to the right
+		this.x = 400 - this.width;
+		this.x_speed = 0;
+	}
 };
 
 Ball.prototype.update = function(paddle1, paddle2) {
@@ -130,3 +159,12 @@ Ball.prototype.render = function () {
 	context.fill();
 };
 
+var keysDown = {};
+
+window.addEventListener("keydown", function(event) {
+	keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+	delete keysDown[event.keyCode];
+});
